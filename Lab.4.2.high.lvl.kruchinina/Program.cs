16554d4a67e949e070e1e63960adfcbe2b6defd7
1 <1@DESKTOP-ACMQRLH>
2023-11-15 19:51:48 +0200

    Добавьте файлы проекта.

---
+++ Lab.4.2.high.lvl.kruchinina/Program.cs
@@ -0,0 +1,24 @@
+﻿//22 var high lvl
+int n = 4; 
+int[,] mas = new int[n, n]; 
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i == j) 
+            mas[i, j] = 1;
+        else if (i < j) 
+            mas[i, j] = 0;
+        else 
+            mas[i, j] = 2;
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(mas[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
